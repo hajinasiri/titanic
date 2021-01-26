bfa643d7a9dc934f3f49d09bcc230f90b98d1609
Shahab <hajinasiri@gmail.com>
2021-01-25 18:02:22 -0800

    cleaning up the coding and modifying readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # titanic
 Kaggle's titanic challenge
+This is the first machine learning challenge I have done. 
+Some data was missing from age column that were filled by averaging the according class's age
+Cabin, name, ticket and passengerId columns were droped.
+sex column and Pclass columns were categorized.
+Compared a few algorithms inlcluding Random Forest and logistic regression.
+Logistic regression showed a better performance 
+After submitting the result to Kaggle my score was 0.7606
+It obviously needs more work to reach higher accuracy and will be revisited later
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 import pandas as pd
-#import seaborn as sns
-#import matplotlib.pyplot as plt
-train_data = pd.read_csv("train.csv")
-#print(train_data)
-test_data = pd.read_csv("test.csv")
-
-
-#sns.heatmap(train_data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
-#plt.show()
-#sns.countplot(x="Survived",data=train_data)
-#plt.show()
-class1 = 38
-class2 = 30
-class3 = 25
-
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 def age_correction(input):
     #This function gets Age and Pclass as element of input and if Age is null
     # replaces them with the average
@@ -29,31 +17,56 @@ def age_correction(input):
             return 25
     else:
        return Age
+
+def children(num):
+    #this function determins if a passenger is a child
+    Age = num[0]
+    male = num[1]
+    if int(Age)<16:
+        return -1
+    else:
+        return int(male)
+test_data = pd.read_csv("test.csv")
+train_data = pd.read_csv("train.csv")
+#sns.heatmap(train_data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+#plt.show()
+#sns.countplot(x="Survived",data=train_data)
+#plt.show()
+class1 = 38
+class2 = 30
+class3 = 25
+
 train_data["Age"] = train_data[["Age", "Pclass"]].apply(age_correction, axis=1)
 train_data.drop('Cabin',axis=1,inplace=True)
-
 train_data.dropna(inplace=True)
 #sns.heatmap(train_data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
 #plt.show()
 Mclass = pd.get_dummies(train_data['Pclass'],drop_first=True)
 sex = pd.get_dummies(train_data['Sex'],drop_first=True)
-embark = pd.get_dummies(train_data['Embarked'],drop_first=True)
-train_data = pd.concat([train_data,sex,embark,Mclass],axis=1)
+#####embark = pd.get_dummies(train_data['Embarked'],drop_first=True)
+#train_data = pd.concat([train_data,sex,embark,Mclass],axis=1)
+train_data = pd.concat([train_data,sex,Mclass],axis=1)
 train_data.drop(['Sex','Embarked','Name','Ticket','Pclass'],axis=1,inplace=True)
 train_data.drop(['PassengerId'],axis=1,inplace=True)
+train_data['male'] = train_data[['Age','male']].apply(children,axis=1)
 y = train_data["Survived"]
 X = train_data.drop('Survived',axis=1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression(C=0.1,max_iter=1000)
+lr = LogisticRegression(C=1,max_iter=1000)
 lr.fit(X_train,y_train)
 prediction = lr.predict(X_test)
 from sklearn.metrics import classification_report
 print(classification_report(y_test,prediction))
+#from sklearn.ensemble import RandomForestClassifier
+#random_forest = RandomForestClassifier(n_estimators=50)
+#random_forest.fit(X_train, y_train)
+#y_pred = random_forest.predict(X_test)
+#print(classification_report(y_test,y_pred))
 
 #from sklearn.model_selection import GridSearchCV
-#grid={"C":np.logspace(-3,3,7), "penalty":["l1","l2"]}# l1 lasso l2 ridge
+#grid={"C":np.logspace(-3,3,7), "penalty":["l2"]}# l1 lasso l2 ridge
 #logreg_cv=GridSearchCV(lr,grid,cv=10)
 #logreg_cv.fit(X_train,y_train)
 #print("tuned hpyerparameters :(best parameters) ",logreg_cv.best_params_)
@@ -62,26 +75,21 @@ print(classification_report(y_test,prediction))
 
 #training LR with the whole training data
 lr = lr.fit(X,y)
-
-
 test_data["Age"] = test_data[["Age", "Pclass"]].apply(age_correction, axis=1)
 test_data.drop('Cabin',axis=1,inplace=True)
-test_null_collumns = test_data.columns[test_data.isna().any()].tolist()
 test_data = test_data.fillna(test_data.mean())
-
 #sns.heatmap(train_data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
 #plt.show()
 Mclass = pd.get_dummies(test_data['Pclass'],drop_first=True)
 sex = pd.get_dummies(test_data['Sex'],drop_first=True)
-embark = pd.get_dummies(test_data['Embarked'],drop_first=True)
-
-test_data = pd.concat([test_data,sex,embark,Mclass],axis=1)
+#embark = pd.get_dummies(test_data['Embarked'],drop_first=True)
+#test_data = pd.concat([test_data,sex,embark,Mclass],axis=1)
+test_data = pd.concat([test_data,sex,Mclass],axis=1)
 test_data.drop(['Sex','Embarked','Name','Ticket','Pclass'],axis=1,inplace=True)
 test_data.drop(['PassengerId'],axis=1,inplace=True)
-
-
-X = test_data
-result = lr.predict(X)
+test_data['male'] = test_data[['Age','male']].apply(children,axis=1)
+data = test_data
+result = lr.predict(data)
 result = pd.DataFrame(result,index=range(892, 1310),columns=['Survived'])
 result.index.names = ['PassengerId']
 result.to_csv('./result.csv')
